ac460fb20c82447cf43936e33ff51a7ab02747a3
Anuj Kumar <anujk7.dev@gmail.com>
2023-12-18 15:29:31 +0530

    committed

--- control.py
+++ control.py
@@ -6,25 +6,24 @@ def read_json(file_path):
         data = json.load(file)
     return data
 
-def create_hotspot(ssid, password):
+def turn_on_hotspot(ssid, password):
     try:
-        subprocess.run(['sudo', 'iw', 'dev', 'wlan0', 'interface', 'add', 'mon0', 'type', 'managed'], check=True)
-        subprocess.run(['sudo', 'ip', 'link', 'set', 'mon0', 'up'], check=True)
-        subprocess.run(['sudo', 'iw', 'mon0', 'ibss', 'join', ssid, '2437', '02:CA:FF:EE:BA:BE'], check=True)
-        subprocess.run(['sudo', 'systemctl', 'start', 'hostapd'], check=True)
-        subprocess.run(['sudo', 'systemctl', 'start', 'dnsmasq'], check=True)
-        subprocess.run(['sudo', 'systemctl', 'start', 'dhclient', 'mon0'], check=True)
-        print('Hotspot created successfully!')
+        subprocess.run(['nmcli', '--version'], check=True, stdout=subprocess.PIPE)
+        subprocess.run(['nmcli', 'device', 'wifi', 'hotspot', 'ifname', 'wlan0', 'con-name', 'Hotspot', 'ssid', ssid, 'password', password], check=True)
+        print('Hotspot turned on successfully!')
     except subprocess.CalledProcessError as e:
-        print('Error:', e)
+        print(f'Error turning on hotspot: {e}')
 
 if __name__ == "__main__":
-    json_data = read_json('data.json')
+    json_data = read_json('./backend/data.json')
     
     ssid = json_data.get('ssid', '')
     password = json_data.get('password', '')
 
     if ssid and password:
-        create_hotspot(ssid, password)
+        turn_on_hotspot(ssid, password)
     else:
         print('SSID or password missing in the JSON file.')
+
+
+
