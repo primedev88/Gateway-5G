bfe3b2f47653184a90aed59362c4380c5f7059f9
primedev88 <anujk7.dev@gmail.com>
2024-01-23 17:19:01 +0530

    commit

--- backend/utils/adapter.js
+++ backend/utils/adapter.js
@@ -1,27 +1,5 @@
 // This module gives the adapter name
 
-// const { exec } = require('child_process');
-
-// const getAdapterName = async () => {
-//     let adapterName = '';
-
-//     await exec('iwconfig', (error, stdout, stderr) => {
-//         if (error) {
-//             console.error('Error in executing command iwconfig ', error);
-//         }
-//         const lines = stdout.split('\n');
-//         lines.forEach((line) => {
-//             if (line.includes('IEEE 802.11')) {
-//                 adapterName = line.split(' ')[0];
-//             }
-//         })
-//     })
-//     return adapterName;
-// }
-
-// module.exports = {
-//     getAdapterName
-// }
 const { exec } = require('child_process');
 
 const getAdapterName = () => {
